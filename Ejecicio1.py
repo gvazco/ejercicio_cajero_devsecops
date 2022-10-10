097b0bcb1cdd5053c12be8158e2875621b38ad49
Gustavo Vázco <gustavovazco@gmail.com>
2022-10-10 00:02:41 -0500

    Update Ejecicio1.py

--- Ejecicio1.py
+++ Ejecicio1.py
@@ -2,12 +2,11 @@
 #                                  Ejercicio 1                                 #
 # ---------------------------------------------------------------------------- #
 
-# Hacer un prorama que simule un cajero automático con un saldo inicial de $1000
+# Hacer un prorama que simule un cajero automático con un saldo inicial de $1000.
 # Al iniciar el programa deberá pedir el siguiente pin: “1235” para autenticar.
 # Si está mal, deberá pedirlo máximo 3 veces.
 # Pasando esas 3 vez el pin no es correcto, el programa deberá terminarse.
-# Si es correcto mostrará un mensaje de bienvenida con tu nombre.
-# y que tenga el siguiente menú de opciones:
+# Si es correcto mostrará un mensaje de bienvenida con tu nombre y el siguiente menú de opciones:
 
 # 1.- Ingresar el dinero en la cuenta
 # 2.- Retirar el dinero de la cuenta
